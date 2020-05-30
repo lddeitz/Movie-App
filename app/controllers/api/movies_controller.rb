c6cbc3345327a52b9b635ca8a5a45609e2432f47
luke <lddeitz@gmail.com>
2020-05-30 12:31:52 -0500

    adds active record queries

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,6 +1,6 @@
 class Api::MoviesController < ApplicationController
   def movie_action
-    @movie = Movie.all
+    @movie = Movie.all.limit(8).offset(4)
     render 'movies.json.jb'
   end
 
@@ -8,4 +8,16 @@ class Api::MoviesController < ApplicationController
     @singlemovie = Movie.find_by(id: 1)
     render 'singlemovie.json.jb'  
   end
+
+  def take
+    @take = Movie.take(2)
+    render 'take.json.jb'
+  end
+
+  def pure
+    @pure = Movie.where("year = 2002").or(Movie.where("year=2019")).or(Movie.where("year=1999"))
+    render 'pure.json.jb'
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
-require "http"
-
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
@@ -11,5 +9,10 @@ Rails.application.routes.draw do
     get "/movies" => "movies#movie_action"
 
     get "/each-movie" => "movies#singlemovie_action"
+
+    get "/take" => "movies#take"
+
+    get "/pure" => "movies#pure"
+
   end
 end
